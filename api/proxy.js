7ced6830b706b21aee4a42bce94176809440bf6b
wumingshi <101516281+bian2022@users.noreply.github.com>
2024-01-17 16:16:03 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://zh.m.wikipedia.org/";//your website url
+  let target = "https://t.me/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
